fad127a0ebc976c95ffbee12af5f0f0e9f5dbfbb
Henrik Christiansen <henpia@gmail.com>
2017-12-31 14:14:59 +0100

    Implemented SQL Repository.

--- MyShop/MyShop.DataAccess.InMemory/InMemoryRepository.cs
+++ MyShop/MyShop.DataAccess.InMemory/InMemoryRepository.cs
@@ -9,13 +9,13 @@ using System.Threading.Tasks;
 
 namespace MyShop.DataAccess.InMemory
 {
-    public class InMemoryRepository<T> : IRepository<T> where T : BaseEntity
+    public class SQLRepository<T> : IRepository<T> where T : BaseEntity
     {
         ObjectCache cache = MemoryCache.Default;
         List<T> items;
         string className;
 
-        public InMemoryRepository()
+        public SQLRepository()
         {
             className = typeof(T).Name;
             items = cache[className] as List<T>;
